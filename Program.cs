79b2f08383891c145b99400265e5b8446f3ce07c
fomenkokipz23-debug <fomenko.k_ipz23@rcit.ukr.education>
2025-09-22 17:26:05 +0300

    Lab1v20

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+class Figure
+{
+    private string name;
+
+    public string Name 
+    { 
+        get => name; 
+        set => name = value; 
+    }
+
+    public double Area 
+    {
+        get 
+        {
+            return name.Length * 3.14;
+        }
+    }
+
+    public Figure(string name)
+    {
+        this.name = name;
+        Console.WriteLine($"Constructor: Figure '{name}' created.");
+    }
+
+    ~Figure()
+    {
+        Console.WriteLine($"Destructor: Figure '{name}' destroyed.");
+    }
+
+    public string GetFigure()
+    {
+        return $"Figure: {name}, Area: {Area}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Figure f1 = new Figure("Circle");
+        Figure f2 = new Figure("Square");
+
+        Console.WriteLine(f1.GetFigure());
+        Console.WriteLine(f2.GetFigure());
+
+        Figure f3 = new Figure("Triangle");
+        Console.WriteLine(f3.GetFigure());
+    }
+}
